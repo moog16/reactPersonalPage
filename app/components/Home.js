03be725d5dbb8fa575550683a843f2808603f13a
Matt Goo <mattgoo@gmail.com>
2015-07-03 20:19:17 -0700

    initial commit for react es6 app

---
+++ app/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <p>hi</p>
+    )
+  }
+});
---
+++ app/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+var RouteHandler = require('react-router').RouteHandler;
+
+class Main extends React.Component{
+  render(){
+    return (
+      <div className="main-container">
+        <h2>This is a header</h2>
+        <RouteHandler />
+      </div>
+    )
+  }
+};
+
+module.exports = Main;
