8834996391ccaae27805a0857e99f66d4026ae83
Matt Goo <mattgoo@gmail.com>
2015-07-23 21:52:20 -0700

    updated .gitignore created readme

--- app/components/Home/SplashPage.js
+++ app/components/Home/SplashPage.js
@@ -6,8 +6,8 @@ class SplashPage extends React.Component {
       <div className="cover--ollantaytambo">
         <div className="u-pt++">
           <div className="layout layout--center">
-            <div className="layout__item u-1/2">Hi there! I am</div>
             <div className="layout__item u-1/2">
+              <div className="text--center">Hi there! I am</div>
               <h1 className="u-m0">Matt Goo</h1>
             </div>
           </div>
