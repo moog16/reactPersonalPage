f8a4cf9b797226b31300be8c0049a6da07753df3
Matt Goo <mattgoo@gmail.com>
2015-07-23 22:23:51 -0700

    added new dear annabelle font updated cover page

--- app/components/Home/SplashPage.js
+++ app/components/Home/SplashPage.js
@@ -3,12 +3,12 @@ import React from 'react';
 class SplashPage extends React.Component {
   render() {
     return (
-      <div className="cover--ollantaytambo">
+      <div className="cover--foggy-northface">
         <div className="u-pt++">
           <div className="layout layout--center">
-            <div className="layout__item u-1/2">
-              <div className="text--center">Hi there! I am</div>
-              <h1 className="u-m0">Matt Goo</h1>
+            <div className="layout__item u-1/2 text--color--light">
+              <div className="text--center text--large">Hi there! I am</div>
+              <h1 className="u-m0 text--center">matt goo</h1>
             </div>
           </div>
         </div>
