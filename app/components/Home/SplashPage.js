9cdb3760391ca8ae24293ce3695389fe2bd0f456
Matt Goo <mattgoo@gmail.com>
2015-07-27 22:35:32 -0700

    vertically aligned and centered the splash page

--- app/components/Home/SplashPage.js
+++ app/components/Home/SplashPage.js
@@ -3,12 +3,11 @@ import React from 'react';
 class SplashPage extends React.Component {
   render() {
     return (
-      <div className="cover--foggy-northface">
-        <div className="u-pt++">
-          <div className="layout layout--center">
-            <div className="layout__item u-1/2 text--color--light text--large text--center">
-              <div className="inline--desk text--center">Hi there! I am </div><h1 className="inline--desk text--center">matt goo</h1>
-            </div>
+      <div className="cover--fish">
+        <div className="layout layout--center layout--flush">
+          <div className="cover__header layout__item u-1/2 text--color--light text--large text--center">
+            <div className="text--center border--main border--bottom--dashed">Hi there! I am </div>
+            <h1 className="text--center text--huge"> matt goo</h1>
           </div>
         </div>
       </div>
---
+++ app/components/Statement.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Statement extends React.Component {
+  render() {
+    return (
+      <div className="cover--foggy-northface">
+        <div className="u-pt++">
+          
+        </div>
+      </div>
+    )
+  }
+};
+
+export default Statement;
